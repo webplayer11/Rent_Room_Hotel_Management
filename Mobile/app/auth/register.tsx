838062d3c2a434080cb2e385ee5cdda22c72b65b
thuydung0511 <tacdung511@gmail.com>
2026-05-13 19:00:11 +0700

    refactor: organize auth routes

---
+++ Mobile/app/auth/register.tsx
@@ -0,0 +1,5 @@
+import RegisterScreen from '../../src/features/auth/RegisterScreen';
+
+export default function RegisterRoute() {
+  return <RegisterScreen />;
+}
--- Mobile/app/index.tsx
+++ Mobile/app/index.tsx
@@ -1,5 +1,5 @@
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-  return <Redirect href="/customer" />; // thay đổi nếu muốn test màn khác 
+  return <Redirect href="/auth/forgot-password" />; // thay đổi nếu muốn test màn khác 
 }
