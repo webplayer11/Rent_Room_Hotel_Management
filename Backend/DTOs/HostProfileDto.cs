6ebeae272a378af83b5c69501bc129908c419c38
hoangf2905 <hoang29052005@gmail.com>
2026-05-19 15:29:03 +0700

    sửa 1 chút

--- Backend/DTOs/HostProfileDto.cs
+++ Backend/DTOs/HostProfileDto.cs
@@ -15,7 +15,6 @@ public class HostProfileDto
     public string? Email { get; set; }
     public string? FullName { get; set; }
     public string? PhoneNumber { get; set; }
-    public DateTime CreatedAt { get; set; }
 }
 
 public class UpdateHostProfileDto
